4f4e6abcb692bb6bdd0a32e2ccbf5f0f462a55c4
caoqi@vpgame.cn <caoqi@vpgame.cn>
2019-12-31 18:23:58 +0800

    feat:添加css模块,feat:添加德州计算对子模块

--- TexasHoldem/index.js
+++ TexasHoldem/index.js
@@ -17,7 +17,7 @@ const TexasHoldem = {
   init: () => { //新建一副扑克
     let poker = [];
     let color = [1,2,3,4] //1：黑桃，2：红心，3：草花， 4：方块
-    for(let i=2; i<15; i++){ //为了计算方便，A算14
+    for(let i=1; i<14; i++){ //为了计算方便，A算14
       color.forEach(item=>{
         poker.push([i,item])
       })
@@ -48,43 +48,157 @@ const TexasHoldem = {
   },  
   sortCards:()=>{
     TexasHoldem.AllCard.forEach(item=>{
-      TexasHoldem.calculateCards(item)
-    })
-   
+      console.log(TexasHoldem.calculateCards(item))
+    })   
   },
   sortNumber: (a,b) => {
-    return a - b
+    return b-a
   },
   unique (arr) {
     return Array.from(new Set(arr))
   },
   calculateCards:(item) => { //计算牌面
-    let number = []; //牌面数字，用来计算是否是顺子
-    let color = []; //牌面花色，用来计算是否是同花
-    item.forEach(item2 => {
-      number.push(item2[0])
-      color.push(item2[1])
-    })
-    number = TexasHoldem.unique(number)
-    if(number.indexOf(14) > -1){
-      number.push(1)
+    if( TexasHoldem.isTonghuaShun(item)){
+      return "同花顺"
+    } else if(TexasHoldem.isShunzi(item)){
+      return ["顺子",TexasHoldem.isShunzi(item)]
+    } else if(TexasHoldem.isTonghua(item)){
+      return "同花"
+    } else{
+      return TexasHoldem.isNum(item)
     }
+  },
+  isTonghuaShun:(item) => { //计算牌面
+    let number = []; //牌面数字，用来计算是否是顺子
+    item.forEach(item2 => {
+      if(item2[0] === 1){
+        number.push(14+item2[1]*100)
+      }
+      number.push(item2[0]+item2[1]*100)
+    })
     number = number.sort(TexasHoldem.sortNumber)
-    color = color.sort(TexasHoldem.sortNumber)
     let oneNumber = 0;
     let max=0
-    for(let i = 0 ; i < 6 ; i ++){
-      if(number[i+1] - number[i] === 1){
+    number.forEach((item, index)=>{
+      if(number[index] - number[index+1] === 1){
         oneNumber ++;
         if(oneNumber === 4){
-          max=number[i+1]
+          max=number[index+1]+4
           return
         }
       }else if(oneNumber > 0 ){
-        oneNumber --;
+        oneNumber = 0;
+      }
+    })
+    if(oneNumber === 4){
+      return max
+    } else {
+      return false
+    }
+  },
+  isShunzi:(item)=> { //计算牌面
+    let number = []; //牌面数字，用来计算是否是顺子
+    item.forEach(item2 => {
+      number.push(item2[0])
+    })
+    number = TexasHoldem.unique(number)
+    if(number.indexOf(1) > -1){
+      number.push(14)
+    }
+    number = number.sort(TexasHoldem.sortNumber)
+    let oneNumber = 0;
+    let max=0
+    for(let i = 0 ; i < 6 ; i ++){
+      if(number[i] - number[i+1] === 1){
+        oneNumber ++;
+        if(oneNumber === 4){
+          max=number[i+1]+4
+          return
+        }
+      }else if(oneNumber > 0 ){
+        oneNumber = 0;
       }
     }
-    console.log(item, number, color, oneNumber)
+    if(oneNumber === 4){
+      return max
+    } else {
+      return false
+    }
+  },
+  isTonghua: (item) => {
+    let color = []; //牌面花色，用来计算是否是同花
+    item.forEach(item2 => {
+      color.push(item2[1])
+    })
+    color = color.sort(TexasHoldem.sortNumber)
+    let oneNumber = 0
+    for(let i = 0 ; i < 6 ; i ++){
+      if(color[i] - color[i+1] === 0){
+        oneNumber ++
+        if(oneNumber === 3){
+          return
+        }
+      }else if(oneNumber > 0 ){
+        oneNumber = 0;
+      }
+    }
+    if(oneNumber === 4){
+      return '同花'
+    } else {
+      return false
+    }
+  },
+  isNum: (item) => {
+    let number = []; //牌面数字，用来计算是否是对子，葫芦，三条等
+    item.forEach(item2 => {
+      number.push(item2[0])
+    })
+    number = number.sort(TexasHoldem.sortNumber)
+    let nowNumber = number[0]
+    let maxNumber = number[0]
+    let oneNumber = {}
+    for(let i = 1 ; i < 6 ; i ++){      
+      if(number[i] - nowNumber === 0){    
+        if(oneNumber[nowNumber]){
+          oneNumber[nowNumber] ++
+        } else {
+          oneNumber[nowNumber] = 2
+        }          
+      }else{
+        nowNumber = number[i]
+      }
+    }
+    let arr = Object.keys(oneNumber)
+    let maxlength = oneNumber[arr[0]] || 0
+    let minlength = 0
+    if(oneNumber[arr[1]]&&oneNumber[arr[1]]>oneNumber[arr[0]]){
+      maxlength = oneNumber[arr[1]]
+      maxNumber = arr[1]
+    } else if (oneNumber[arr[1]]){
+      minlength = oneNumber[arr[1]]
+    } else if (oneNumber[arr[0]]) {
+      maxNumber = arr[0]
+    }
+    switch(maxlength){
+      case 0:
+        return ["单个",maxNumber]
+      case 2:
+        if(minlength === 0){
+          return ["对子",maxNumber]
+        } else {
+          return ["两对",maxNumber]
+        }
+      case 3:
+        if(minlength === 0){
+          return ["三条",maxNumber]
+        } else {
+          return ["葫芦",maxNumber]
+        }        
+      case 4:
+        return ["四个",maxNumber]
+      default:
+        break
+    }
   }
 }
 
